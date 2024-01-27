6dd13786ba99fc6b930fe4767aea6900e68add98
Denis <denisrubel40@gmail.com>
2024-01-27 18:55:52 +0200

    Added a Model for Products and their display on the page

---
+++ CloudShop/CloudShop.Client/Data/ProductContext.cs
@@ -0,0 +1,71 @@
+﻿using CloudShop.Client.Models;
+
+namespace CloudShop.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+                Id = "1",
+                Name = "IPhone X",
+                Description =
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                ImageFile = "product-1.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Id = "2",
+                Name = "Samsung 10",
+                Description =
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                ImageFile = "product-2.png",
+                Price = 840.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Id = "3",
+                Name = "Huawei Plus",
+                Description =
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                ImageFile = "product-3.png",
+                Price = 650.00M,
+                Category = "White Appliances"
+            },
+            new Product()
+            {
+                Id = "4",
+                Name = "Xiaomi Mi 9",
+                Description =
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                ImageFile = "product-4.png",
+                Price = 470.00M,
+                Category = "White Appliances"
+            },
+            new Product()
+            {
+                Id = "5",
+                Name = "HTC U11+ Plus",
+                Description =
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                ImageFile = "product-5.png",
+                Price = 380.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Id = "6",
+                Name = "LG G7 ThinQ New8",
+                Description =
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                ImageFile = "product-6.png",
+                Price = 240.00M,
+                Category = "Home Kitchen"
+            }
+        };
+    }
+}
--- CloudShop/CloudShop.Client/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CloudShop.Client.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
